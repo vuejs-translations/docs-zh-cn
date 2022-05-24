b848738e1d768cead0aa2c884193d5d030d33b43
Jinjiang <zhaojinjiang@me.com>
2022-05-24 09:18:22 +0800

    translate hello-world example

--- src/examples/src/hello-world/App/composition.js
+++ src/examples/src/hello-world/App/composition.js
@@ -2,11 +2,9 @@ import { ref } from 'vue'
 
 export default {
   setup() {
-    // A "ref" is a reactive data source that stores a value.
-    // Technically, we don't need to wrap the string with ref()
-    // in order to display it, but we will see in the next
-    // example why it is needed if we ever intend to change
-    // the value.
+    // “ref”是用来存储值的响应式数据源。
+    // 理论上我们在展示该字符串的时候不需要将其包装在 ref() 中，
+    // 但是在下一个示例中更改这个值的时候，我们就需要它了。
     const message = ref('Hello World!')
 
     return {
