0124533db8d0be0e39cf659306ee174b1088682b
wxsm <wxsms@foxmail.com>
2022-09-19 17:53:33 +0800

    chore: script to generate sync pr content (#542)
    * chore: script to generate sync pr content
    
    * chore: open pr
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,23 +1,59 @@
-/**
- * usage: npm run sync
- *
- * find the latest synced commit hash from local git by `sync #hash` pattern,
- * and open a browser tab of github.com/vuejs/docs,
- * with the compare between this hash and latest master branch.
- * save a little bit time between finding it manually.
- *
- * https://github.com/vuejs/docs-next-zh-cn/pull/728
- */
-const git = require('simple-git')();
+const { Command } = require('commander');
+const SimpleGit = require('simple-git');
+const program = new Command();
 const open = require('open');
 
-(async () => {
-  const history = await git.log()
-  const latestSync = history.all.find(v => /^sync #\w{7}\s.+$/i.test(v.message))?.message
+async function getLatestSyncHash () {
+  const git = SimpleGit();
+  const history = await git.log(['origin/main']);
+  const latestSync = history.all.find(v => /^sync #\w{7}\w?\s?$/i.test(v.body))?.body?.trim();
+  // console.log(history.all[0])
   if (!latestSync) {
-    console.log('No hash found. Are there any commit with `sync #hash` message in git history?');
-    return;
+    throw new Error('No hash found. Are there any commit with `sync #hash` message in git history?');
   }
-  const latestSyncHash = latestSync.match(/#(\w+)\s/)[1]
-  open(`https://github.com/vuejs/docs/compare/${latestSyncHash}...master`)
-})()
+  // console.log(latestSync)
+  return latestSync.match(/#(\w{7}\w?)/)[1];
+}
+
+async function getLatestUpstreamHash () {
+  const git = SimpleGit();
+  const latestUpstreamHash = await git.show(['-s', '--format="%h"', 'origin/upstream']);
+  return latestUpstreamHash.trim().replace(/"/g, '');
+}
+
+program
+  .name('sync')
+  .description('The sync helper CLI');
+
+program.command('compare')
+  .description('Open the github website to compare diffs to sync.')
+  .action(async () => {
+    open(`https://github.com/vuejs/docs/compare/${await getLatestSyncHash()}...${await getLatestUpstreamHash()}`);
+  });
+
+program.command('pr')
+  .description('Generate sync PR\'s title and content.')
+  .option('-o, --open', 'directly open link to create pr (sync branch->main branch)')
+  .action(async (options) => {
+    const latestUpstreamHash = await getLatestUpstreamHash();
+    const latestSyncHash = await getLatestSyncHash();
+
+    const title = `Sync #${latestUpstreamHash}`;
+    const body = `## Description of Problem
+
+https://github.com/vuejs/docs/compare/${latestSyncHash}...${latestUpstreamHash}
+`;
+
+    console.log(`
+--- PR Title ---:
+${title}
+
+--- PR Content ---:
+${body}`);
+
+    if (options.open) {
+      open(`https://github.com/vuejs-translations/docs-zh-cn/compare/main...sync?quick_pull=1&title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}&labels=${encodeURIComponent('从英文版同步,请使用 merge commit 合并')}`);
+    }
+  });
+
+program.parse();
