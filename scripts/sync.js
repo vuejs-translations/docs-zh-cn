3570bc6c8f60d41a843c30ebc9036596f882bc76
guokairui <wxsms@foxmail.com>
2022-09-19 09:24:10 +0800

    update

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,17 +1,60 @@
-/**
- * usage: pnpm run sync:viewdiff
- *
- * find the latest synced commit hash from local git by `sync #hash` pattern,
- * and open a browser tab of github.com/vuejs/docs,
- * with the compare between this hash and latest master branch.
- * save a little bit time between finding it manually.
- *
- * https://github.com/vuejs/docs-next-zh-cn/pull/728
- */
+const { Command } = require('commander');
+const SimpleGit = require('simple-git');
+const program = new Command();
 const open = require('open');
-const { getLatestSyncHash } = require('./utils');
 
-(async () => {
-  const latestSyncHash = await getLatestSyncHash();
-  open(`https://github.com/vuejs/docs/compare/${latestSyncHash}...main`);
-})();
+async function getLatestSyncHash () {
+  const git = SimpleGit();
+  const history = await git.log(['origin/main']);
+  const latestSync = history.all.find(v => /^sync #\w{7}\w?\s?$/i.test(v.body))?.body?.trim();
+  // console.log(history.all[0])
+  if (!latestSync) {
+    throw new Error('No hash found. Are there any commit with `sync #hash` message in git history?');
+  }
+  // console.log(latestSync)
+  return latestSync.match(/#(\w{7}\w?)/)[1];
+}
+
+async function getLatestUpstreamHash() {
+  const git = SimpleGit();
+  const latestUpstreamHash = await git.show(['-s', '--format="%h"', 'origin/upstream']);
+  return latestUpstreamHash.trim().replace(/"/g, '');
+}
+
+program
+  .name('sync')
+  .description('The sync helper CLI')
+  .version('0.8.0');
+
+program.command('compare')
+  .description('Open the github website to compare diffs to sync.')
+  .action(async () => {
+    open(`https://github.com/vuejs/docs/compare/${await getLatestSyncHash()}...${await getLatestUpstreamHash()}`);
+  });
+
+program.command('pr')
+  .description('Generate sync PR\'s title and content.')
+  .option('--open', 'directly open link to create pr (sync branch->main branch)')
+  .action(async (options) => {
+    const latestUpstreamHash = await getLatestUpstreamHash();
+    const latestSyncHash = await getLatestSyncHash();
+
+    const title = `Sync #${latestUpstreamHash}`;
+    const body = `## Description of Problem
+
+https://github.com/vuejs/docs/compare/${latestSyncHash}...${latestUpstreamHash}
+`;
+
+    console.log(`
+--- PR Title ---:
+${title}
+
+--- PR Content ---:
+${body}`);
+
+    if (options.open) {
+      open(`https://github.com/vuejs-translations/docs-zh-cn/compare/main...sync?quick_pull=1&title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}`);
+    }
+  });
+
+program.parse();
