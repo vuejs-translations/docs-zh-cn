c935d88fe1b867b65ba843213971883f9a257f8e
ponjs <39897744+ponjs@users.noreply.github.com>
2022-10-31 11:26:11 +0800

    themes page translation (#593)
    * themes page translation
    
    * Update src/ecosystem/themes/themes.json
    
    Co-authored-by: wxsm <wxsms@foxmail.com>

--- src/ecosystem/themes/ThemePage.vue
+++ src/ecosystem/themes/ThemePage.vue
@@ -7,14 +7,14 @@ import ThemeContact from './ThemeContact.vue'
 <template>
   <div class="ThemePage">
     <ThemeHero>
-      <template #title>Themes</template>
-      <template #lead>Check out the themes, UI Kits, and plugins. you can see how a real world application is built with Vue by our partners.</template>
+      <template #title>主题</template>
+      <template #lead>查看主题、UI Kits 和插件。你可以看到我们的合作伙伴是如何用 Vue 构建一个真实世界的应用。</template>
     </ThemeHero>
 
     <ThemeList />
 
     <ThemeContact>
-      Want to feature your themes here? <a class="link" href="mailto:evan@vuejs.org?subject=Theme+affiliation">Contact us!</a>
+      想在这里介绍你的主题吗？ <a class="link" href="mailto:evan@vuejs.org?subject=Theme+affiliation">联系我们!</a>
     </ThemeContact>
   </div>
 </template>
