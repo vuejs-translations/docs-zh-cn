2756d09722e078ea24148d45dab617d9471b8083
Evan You <yyx990803@gmail.com>
2022-11-28 11:52:43 +0800

    chore: fix sponsor logic

--- .vitepress/theme/components/sponsors.ts
+++ .vitepress/theme/components/sponsors.ts
@@ -27,16 +27,6 @@ export const base = `https://sponsors.vuejs.org`
 export const load = async () => {
   if (!pending.value) {
     pending.value = true
-    const raw = await (await fetch(`${base}/data.json`)).json()
-
-    // custom dcloud randomization logic
-    raw.platinum_china.forEach((s: Sponsor) => {
-      if (s.name === 'DCloud') {
-        s.height = '70px'
-        if (Math.random() >= 0.5) s.img = 'dcloud2.png'
-      }
-    })
-
-    data.value = raw
+    data.value = await (await fetch(`${base}/data.json`)).json()
   }
 }
