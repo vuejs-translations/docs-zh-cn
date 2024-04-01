7d7b6946bd7f1fc8cc6302ab19370b1ee673d84e
warmthsea <2586244885@qq.com>
2024-04-01 11:33:34 +0800

    fun: not 2024-04-01

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -16,7 +16,8 @@ onMounted(async () => {
 <template>
   <section id="hero">
     <h1 class="tagline">
-      <span class="accent">威优易</span>
+      <span class="accent">渐进式</span>
+      <br />JavaScript 框架
     </h1>
     <p class="description">
       易学易用，性能出色，适用场景丰富的 Web 前端框架。
@@ -92,20 +93,20 @@ onMounted(async () => {
 
   <section id="highlights" class="vt-box-container">
     <div class="vt-box">
-      <h2 class="accent">威</h2>
-      <p>
-        丰富的、可渐进式集成的生态系统，可以根据应用规模在库和框架间切换自如。
-      </p>
-    </div>
-    <div class="vt-box">
-      <h2 class="accent">优</h2>
-      <p>经过编译器优化、完全响应式的渲染系统，几乎不需要手动优化。</p>
-    </div>
-    <div class="vt-box">
-      <h2 class="accent">易</h2>
+      <h2>易学易用</h2>
       <p>
         基于标准 HTML、CSS 和 JavaScript 构建，提供容易上手的 API
         和一流的文档。
+      </p>
+    </div>
+    <div class="vt-box">
+      <h2>性能出色</h2>
+      <p>经过编译器优化、完全响应式的渲染系统，几乎不需要手动优化。</p>
+    </div>
+    <div class="vt-box">
+      <h2>灵活多变</h2>
+      <p>
+        丰富的、可渐进式集成的生态系统，可以根据应用规模在库和框架间切换自如。
       </p>
     </div>
   </section>
@@ -271,12 +272,11 @@ html:not(.dark) .accent,
 
 #highlights h2 {
   font-weight: 600;
-  font-size: 48px;
+  font-size: 20px;
   letter-spacing: -0.4px;
   color: var(--vt-c-text-1);
   transition: color 0.5s;
-  padding: 0.75em 0;
-  width: 1em;
+  margin-bottom: 0.75em;
 }
 
 #highlights p {
